69d174fccf079600d8a6b19bc85f137a30b923f6
Edwin <edwinacubillos@gmail.com>
2015-10-19 18:16:05 -0500

    Initial commit Esqueleto para las tres aplicaciones de clase

---
+++ app/src/main/java/com/edwinacubillos/sesion10/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.edwinacubillos.sesion10;
+
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ notificacion/src/main/java/com/edwinacubillos/notificacion/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.edwinacubillos.notificacion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Edwin on 19/10/2015.
+ */
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
